bf866794c7d73578e67f361f38f6e02e46e1413e
김성호 <nareun130@gmail.com>
2023-11-28 17:34:54 +0900

    feat : 백엔드 프로젝트 생성

---
+++ back-end/how-watch/src/main/java/com/nareun/howwatch/HowWatchApplication.java
@@ -0,0 +1,13 @@
+package com.nareun.howwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowWatchApplication.class, args);
+	}
+
+}
---
+++ back-end/how-watch/src/test/java/com/nareun/howwatch/HowWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nareun.howwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
